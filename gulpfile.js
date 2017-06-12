f0b441b1e98e582ac077ac6db7e090d0e0c0a89a
JWood <1987boxior@gmail.com>
2017-06-12 15:14:54 +0300

    added structure

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ var browserSync = require('browser-sync'),
     sass = require('gulp-sass');
 
 gulp.task('sass', function() {
-  return gulp.src('app/sass/**/*.sass')
+  return gulp.src('app/sass/style.sass')
   .pipe(sass())
   .pipe(autoprefixer(['last 15 versions', '>1%', 'ie 8', 'ie 7'], {cascade: true}))
   .pipe(gulp.dest('app/css'))
